d29925cdb98961e946502f9930965f65a121f038
MaximTishkin <edlite@yandex.ru>
2025-06-13 16:56:22 +0300

    Третий вариант - чеки не переворачиваются (верно), но есть ошибка при повторном запросе

--- src/main/java/com/receiptanalyzer/service/ImagePreprocessor.java
+++ src/main/java/com/receiptanalyzer/service/ImagePreprocessor.java
@@ -64,42 +64,80 @@ public class ImagePreprocessor {
 
     private Mat deskewImage(Mat binary) {
         try {
-            // Находим все ненулевые точки
+            // Создаем копию входного изображения
+            Mat binaryClone = binary.clone();
+            
+            // Находим все контуры
             MatVector contours = new MatVector();
             Mat hierarchy = new Mat();
-            findContours(binary.clone(), contours, hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);
+            findContours(binaryClone, contours, hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);
+            binaryClone.release();
 
-            // Находим самый большой контур
-            double maxArea = 0;
-            RotatedRect bestRect = null;
+            try {
+                // Находим самый большой контур
+                double maxArea = 0;
+                RotatedRect bestRect = null;
+                double imageArea = binary.size().width() * binary.size().height();
 
-            for (long i = 0; i < contours.size(); i++) {
-                Mat contour = contours.get(i);
-                double area = contourArea(contour);
-                if (area > maxArea) {
-                    maxArea = area;
-                    bestRect = minAreaRect(contour);
+                for (long i = 0; i < contours.size(); i++) {
+                    Mat contour = contours.get(i);
+                    double area = contourArea(contour);
+                    
+                    // Проверяем, что площадь контура достаточно большая
+                    // (не менее 10% от площади изображения)
+                    if (area > maxArea && area > imageArea * 0.1) {
+                        maxArea = area;
+                        bestRect = minAreaRect(contour);
+                    }
                 }
+
+                if (bestRect != null) {
+                    double angle = bestRect.angle();
+                    
+                    // Нормализуем угол в диапазон [-45, 45]
+                    while (angle < -45) {
+                        angle += 90;
+                    }
+                    while (angle > 45) {
+                        angle -= 90;
+                    }
+
+                    // Применяем коррекцию наклона только если угол в допустимом диапазоне
+                    if (Math.abs(angle) > 0.5 && Math.abs(angle) < 45) {
+                        Point2f center = bestRect.center();
+                        Mat rotMatrix = getRotationMatrix2D(center, angle, 1.0);
+                        Mat rotated = new Mat();
+                        Size size = binary.size();
+                        
+                        // Вычисляем новый размер изображения после поворота
+                        double cos = Math.abs(Math.cos(Math.toRadians(angle)));
+                        double sin = Math.abs(Math.sin(Math.toRadians(angle)));
+                        int newWidth = (int) (size.width() * cos + size.height() * sin);
+                        int newHeight = (int) (size.width() * sin + size.height() * cos);
+                        
+                        // Корректируем матрицу преобразования
+                        rotMatrix.ptr(0, 2).putDouble(rotMatrix.ptr(0, 2).getDouble() + (newWidth - size.width()) / 2);
+                        rotMatrix.ptr(1, 2).putDouble(rotMatrix.ptr(1, 2).getDouble() + (newHeight - size.height()) / 2);
+                        
+                        // Применяем поворот
+                        warpAffine(binary, rotated, rotMatrix, new Size(newWidth, newHeight));
+                        rotMatrix.release();
+                        return rotated;
+                    }
+                }
+
+                return binary;
+            } finally {
+                // Освобождаем ресурсы
+                hierarchy.release();
+                for (long i = 0; i < contours.size(); i++) {
+                    Mat contour = contours.get(i);
+                    if (contour != null && !contour.isNull()) {
+                        contour.release();
+                    }
+                }
+                contours.deallocate(); // Используем deallocate вместо release для MatVector
             }
-
-            if (bestRect != null) {
-                double angle = bestRect.angle();
-                if (angle < -45) {
-                    angle = 90 + angle;
-                }
-
-                // Применяем коррекцию наклона только если угол больше 1 градуса
-                if (Math.abs(angle) > 1.0) {
-                    Point2f center = bestRect.center();
-                    Mat rotMatrix = getRotationMatrix2D(center, angle, 1.0);
-                    Mat rotated = new Mat();
-                    Size size = binary.size();
-                    warpAffine(binary, rotated, rotMatrix, size);
-                    return rotated;
-                }
-            }
-
-            return binary;
         } catch (Exception e) {
             return binary;
         }
