59ef55b6191a431d7a3bad1514ca76758f018466
MaximTishkin <edlite@yandex.ru>
2025-06-14 12:42:52 +0300

    Третий вариант - обработка ошибок, ответ клиенту

--- src/main/java/com/receiptanalyzer/controller/OcrController.java
+++ src/main/java/com/receiptanalyzer/controller/OcrController.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,5 +62,14 @@ public class OcrController {
     @FunctionalInterface
     private interface OcrSupplier<T> {
         T get() throws Exception;
+    }
+
+    /**
+     * Глобальный обработчик ошибок для OCR endpoint'ов.
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        log.error("Ошибка при обработке OCR запроса", e);
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 } 
