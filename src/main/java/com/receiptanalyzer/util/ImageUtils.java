96dc4676d9be39645c71230c6859549d15751640
MaximTishkin <edlite@yandex.ru>
2025-06-14 12:40:26 +0300

    Третий вариант - добавил работу с pdf

--- src/main/java/com/receiptanalyzer/util/ImageUtils.java
+++ src/main/java/com/receiptanalyzer/util/ImageUtils.java
@@ -7,11 +7,36 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
 public class ImageUtils {
 
-public static BufferedImage multipartFileToBufferedImage(MultipartFile file) throws IOException {
-        return ImageIO.read(file.getInputStream());
+    public static BufferedImage multipartFileToBufferedImage(MultipartFile file) throws IOException {
+        String filename = file.getOriginalFilename();
+        if (filename == null) {
+            throw new IOException("Имя файла не указано");
+        }
+        String lower = filename.toLowerCase();
+        if (lower.endsWith(".jpg") || lower.endsWith(".jpeg")) {
+            BufferedImage img = ImageIO.read(file.getInputStream());
+            if (img == null) {
+                throw new IOException("Не удалось прочитать изображение. Проверьте корректность файла.");
+            }
+            return img;
+        } else if (lower.endsWith(".pdf")) {
+            try (PDDocument document = PDDocument.load(file.getInputStream())) {
+                PDFRenderer pdfRenderer = new PDFRenderer(document);
+                // Рендерим первую страницу PDF в изображение (dpi = 300 для качества)
+                BufferedImage img = pdfRenderer.renderImageWithDPI(0, 300);
+                if (img == null) {
+                    throw new IOException("Не удалось извлечь изображение из PDF.");
+                }
+                return img;
+            }
+        } else {
+            throw new IOException("Поддерживаются только файлы JPG и PDF");
+        }
     }
 
     public static void saveImageToFile(BufferedImage image, String prefix) throws IOException {
